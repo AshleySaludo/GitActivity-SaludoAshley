2be9f3118ccf45bb2ee278d64e4dfaa288afad92
SaludoAshley <ashleycanoy13@gmail.com>
2022-03-14 17:59:17 +0800

    My Educational Background added

--- git-python-saludo.py
+++ git-python-saludo.py
@@ -1,3 +1,7 @@
 print ("Ashley C. Saludo")
 print ("Mat. Gasan, Gasan, Marinduque")
 print ("April 1, 2002")
+
+print ("Marinduque State College")
+print ("Bachelor of Science in Information Technology ")
+print ("BSI/T 2C, 2021-2022")
